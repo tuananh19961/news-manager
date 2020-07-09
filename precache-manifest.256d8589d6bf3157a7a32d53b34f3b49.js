1561a795a42aa4222361c86a3f1579ffee090785
Tuấn Anh <tuananhcntt96@gmail.com>
2020-07-09 17:58:47 +0700

    Updates

---
+++ precache-manifest.256d8589d6bf3157a7a32d53b34f3b49.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "745ba5efc4cb404b89bf",
+    "url": "/news-manager/static/css/main.953d64e3.chunk.css"
+  },
+  {
+    "revision": "745ba5efc4cb404b89bf",
+    "url": "/news-manager/static/js/main.745ba5ef.chunk.js"
+  },
+  {
+    "revision": "886bf96e2df9603d6a7b",
+    "url": "/news-manager/static/css/1.9e50cc87.chunk.css"
+  },
+  {
+    "revision": "886bf96e2df9603d6a7b",
+    "url": "/news-manager/static/js/1.886bf96e.chunk.js"
+  },
+  {
+    "revision": "fa82039eed46d8f81885",
+    "url": "/news-manager/static/js/runtime~main.fa82039e.js"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/news-manager/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/news-manager/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/news-manager/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/news-manager/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "d60897a0238312ca6978901c65670bd2",
+    "url": "/news-manager/index.html"
+  }
+];
